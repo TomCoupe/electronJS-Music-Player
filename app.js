b82da9fe295e1364e10ca34299f243ca305be634
Tom Coupe <thomas.coupe@yahoo.co.uk>
2021-07-19 19:02:39 +0100

    trying to allow for inspect element. works however background goes black

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('./bootstrap');
 window.Vue = require('vue');
 
-
+Vue.component('tester', require('./components/Tester.vue').default);
 
 const app = new Vue({
     el: '.main',
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
+const contextMenu = require('electron-context-menu');
 const path = require('path')
 
 function createWindow () {
@@ -40,6 +41,35 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+contextMenu({
+	prepend: (defaultActions, parameters, browserWindow) => [
+		{
+			label: 'Rainbow',
+			// Only show it when right-clicking images
+			visible: parameters.mediaType === 'image'
+		},
+		{
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: parameters.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(parameters.selectionText)}`);
+			}
+		}
+	]
+});
+
+let mainWindow;
+(async () => {
+	await app.whenReady();
+
+	mainWindow = new BrowserWindow({
+		webPreferences: {
+			spellcheck: true
+		}
+	});
+})();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
