b8c27aa34790140addf2d8c62624bd9951be4c39
Oli Legg <oliglegg@hotmail.co.uk>
2021-07-20 00:46:22 +0100

    forgot components

---
+++ components/Tester.vue
@@ -0,0 +1,20 @@
+<template>
+    <button v-on:click="testfunc">test works</button>
+</template>
+<script>
+export default {
+    name: "Tester.vue",
+    data () {
+        return {
+            //js variables declared here
+        }
+    },
+    methods: {
+        //js functions declared here
+
+        testfunc() {
+            alert("test");
+        }
+    }
+}
+</script>
