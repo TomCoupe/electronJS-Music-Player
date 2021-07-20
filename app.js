54c2df3e0401c8618492feeb27d796f56cab3273
Tom Coupe <thomas.coupe@yahoo.co.uk>
2021-07-20 14:11:08 +0100

    vue works with ts code

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-require('./bootstrap');
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process unless
+// nodeIntegration is set to true in webPreferences.
+// Use preload.js to selectively enable features
+// needed in the renderer process.
+
+// require('./bootstrap');
 window.Vue = require('vue');
 
 Vue.component('tester', require('./components/Tester.vue').default);
